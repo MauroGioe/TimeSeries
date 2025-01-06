c298df844985e5c0b34c96d39d8a7adda281c2ad
mauro <maurogioe91@libero.it>
2025-01-06 22:51:50 +0100

    memory error if you don't put a limit to the features

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,7 +11,7 @@ train = data[data['Datetime'] < '2016-01-01']
 test = data[data['Datetime'] >= '2016-01-01']
 
 train_features_ext = extract_features(train, column_id = "id", column_sort = 'Datetime', column_value = "AEP_MW" ,
-                                  n_jobs = 10)
+                                  n_jobs = 10, default_fc_parameters = EfficientFCParameters())
 
 train_features_ext = train_features_ext.dropna(axis=1, how='all')
 train_features_ext = select_features(train_features_ext, train["AEP_MW"])
